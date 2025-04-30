63b3e3c0e4074ac9aa2ec39c7600cdd08575a041
Carlos Adryan <carlosadryana@gmail.com>
2025-04-30 13:58:44 -0300

    Age problem

---
+++ problems/age-problem/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string name1, name2;
+    int age1, age2;
+    double average;
+
+    cout << "First person data: " << endl;
+    cout << "Name: ";
+    getline (cin, name1);
+    cout << "Age: ";
+    cin >> age1;
+
+    cout << "Second person data" << endl;
+    cout << "Name: ";
+    cin.ignore(INT_MAX, '\n');
+    getline (cin, name2);
+    cout << "Age: ";
+    cin >> age2;
+
+    average = (age1 + age2) / 2.0;
+
+    cout << fixed << setprecision(1);
+    cout << "The average age of "<< name1 << " and " << name2 << " is " << average << " years" << endl;
+
+    return 0;
+}
